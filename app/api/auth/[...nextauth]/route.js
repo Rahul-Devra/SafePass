fd5cb09338a1138405ce04a1489469f28adf7851
Rahul-Devra <devrarahul556@gmail.com>
2024-06-30 09:22:22 +0530

    Import User

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import User from '../models/User';
-
+import User from "@/app/models/User";
 import connectDB from "@/app/db/connectDB";
 
 export const authOptions = {

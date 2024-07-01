2ebc3c396c339492ab1dec9171f0065eda4c9303
Rahul-Devra <devrarahul556@gmail.com>
2024-07-01 13:57:40 +0530

    Footer: Removing Personal Details

--- components/Footer.js
+++ components/Footer.js
@@ -8,11 +8,11 @@ const Footer = () => {
         <div className="flex  -mx-4">
           <div className="w-full md:w-1/3 px-4 mb-2 md:mb-0"> 
             <h3 className="text-lg md:text-xl font-semibold mb-2">Contact Us</h3> 
-            <p className="text-sm md:text-base"> Email: devrarahul750@gmail.com</p>
+            <p className="text-sm md:text-base"> Email: passop@gmail.com</p>
           </div>
           <div className="w-full md:w-1/3 px-4 mb-2 md:mb-0"> 
             <h3 className="text-lg md:text-xl font-semibold mb-2">Stay Connected</h3> 
-            <p className="text-sm md:text-base">Follow us on <a href="https://github.com/Rahul-Devra/PassOP" className="text-blue-400 hover:text-blue-600">GitHub</a> and <a href="https://www.linkedin.com/company/passop" className="text-blue-400 hover:text-blue-600">LinkedIn</a></p>
+            <p className="text-sm md:text-base">Follow us on <a href="https://github.com/" className="text-blue-400 hover:text-blue-600">GitHub</a> and <a href="https://www.linkedin.com/" className="text-blue-400 hover:text-blue-600">LinkedIn</a></p>
           </div>
           <div className="w-full md:w-1/3 px-4 mb-2 md:mb-0">
             <h3 className=" text-lg md:text-xl font-semibold mb-2">Legal</h3>

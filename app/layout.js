34ca90b77165fae2d6c1c4510d9517f3d3a11a45
Rahul-Devra <devrarahul556@gmail.com>
2025-01-27 14:14:32 +0530

    bg opacity changes

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "SafePass - Securely Manage Your Passwords!",
   description: "Your Trusted Password Management Solution.",
-  
 };
 
 export default function RootLayout({ children }) {
@@ -18,21 +17,27 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <title>{metadata.title}</title>
-        <description>{metadata.description}</description>
+        {/* Corrected meta tag for description */}
+        <meta name="description" content={metadata.description} />
         <link rel="icon" sizes="96x96" href="/icon.png" />
       </Head>
-      <body className=" relative text-white">
-      <video
-        autoPlay
-        muted
-        loop
-        className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
-      >
-        <source src="/bg.mp4" type="video/mp4" />
-      </video>
+      <body className="relative text-white">
+        {/* Background Video */}
+        <video
+          autoPlay
+          muted
+          loop
+          className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
+        >
+          <source src="/bg.mp4" type="video/mp4" />
+        </video>
+        
+        {/* Dark Overlay */}
+        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-70 z-[-1]"></div>
+
         <SessionWrapper>
           <Navbar />
-          <div className="min-h-[83.3vh] ">{children}</div>
+          <div className="min-h-[83.3vh]">{children}</div>
           <Footer />
         </SessionWrapper>
       </body>
